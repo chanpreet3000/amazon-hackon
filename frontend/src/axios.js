c9901a26bfdd653d7db7210eca2ab35d5e9c7936
Chanpreet Singh <chanpreet3000@gmail.com>
2023-10-23 23:49:47 +0530

    env updated

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: process.env.REACT_APP_BASE_URL,
   withCredentials: true,
 });
